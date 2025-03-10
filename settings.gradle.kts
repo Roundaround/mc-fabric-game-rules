224d00c24b722e483ce2cb731eec030ebe8e91cb
Roundaround <roundaroundna@gmail.com>
2025-03-10 16:13:45 -0400

    Integrate with mixing auto registration and fabric.mod.json improvemetns

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement {
   repositories {
     gradlePluginPortal()
-    maven("https://maven.fabricmc.net/")
+    maven("https://maven.fabricmc.net/") { name = "Fabric" }
+    maven("https://repo.spongepowered.org/maven/") { name = "Mixin" }
 //    maven("https://maven.rnda.dev/snapshots/")
     mavenLocal()
   }
