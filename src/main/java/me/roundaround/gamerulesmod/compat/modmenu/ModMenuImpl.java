f055b5c9ea6db008509d7fc216e2beb65e912067
Roundaround <roundaroundna@gmail.com>
2025-03-15 12:58:45 -0400

    Don't show edit rules screen if server is not running mod

--- src/main/java/me/roundaround/gamerulesmod/compat/modmenu/ModMenuImpl.java
+++ src/main/java/me/roundaround/gamerulesmod/compat/modmenu/ModMenuImpl.java
@@ -3,7 +3,9 @@ package me.roundaround.gamerulesmod.compat.modmenu;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.roundaround.gamerulesmod.client.gui.screen.GameRuleScreen;
+import me.roundaround.gamerulesmod.network.Networking;
 import me.roundaround.roundalib.gradle.api.annotation.Entrypoint;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 
 @Entrypoint(Entrypoint.MOD_MENU)
@@ -13,6 +15,9 @@ public class ModMenuImpl implements ModMenuApi {
     return (screen) -> {
       MinecraftClient client = MinecraftClient.getInstance();
       if (client == null || client.world == null || client.player == null) {
+        return null;
+      }
+      if (!ClientPlayNetworking.canSend(Networking.FetchC2S.ID)) {
         return null;
       }
       return new GameRuleScreen(screen);
